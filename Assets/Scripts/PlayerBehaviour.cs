efb3fdc234946d16545fcf7178dd1c2e663f4c26
CAMA JUAN IGNACIO <46956340@ort>
2022-05-26 09:09:46 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject spawnpoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject clon;
+            clon = Instantiate(prefab);
+            clon.transform.position = spawnpoint.transform.position;
+        }
+    }
+}
